fb6b4f609eddd671276ef12aaa112377249d2377
SpiritualGrowthClub <hpgilmore@msn.com>
2026-07-13 16:36:22 -0700

    Add files via upload

--- meetings.js
+++ meetings.js
@@ -7741,7 +7741,7 @@ const meetings = {
         {
           type: "image",
           label: "Happy 2 CD Song List",
-          src: "topic-images/202209-cd-happy2-song-list.png" 
+          src: "topic-images/202209-cd-happy2-song-list.jpg" 
         },
         
    
